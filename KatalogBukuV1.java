0857d73a1d1468d115d9821e0cbffa78ef64046c
ren5602 <naziz5602@gmail.com>
2023-11-16 18:14:58 +0700

    versi awal Katalog

--- KatalogBukuV1.java
+++ KatalogBukuV1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class KatalogBuku {
+public class KatalogBukuV1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String namaBuku;
