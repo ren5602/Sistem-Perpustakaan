23d37022ce5122ca67e464e826d8729d7c1fc0cf
geriiy <gheriyaj@gmail.com>
2023-11-16 18:39:38 +0700

    revisi tampilan katalog

--- KatalogBuku.java
+++ KatalogBuku.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+
 public class KatalogBuku {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int maxBuku = 100, maxAtributBuku = 7, jumlahBuku=0;
+        int maxBuku = 100, maxAtributBuku = 7, jumlahBuku = 0;
         String[][] perpustakaan = new String[maxBuku][maxAtributBuku];
 
         System.out.println("\n______________________________");
@@ -27,95 +28,72 @@ public class KatalogBuku {
         int kategori = input.nextInt();
 
         if (jumlahBuku > 0) {
-            while (true) {
-                for (int i = 0; i < jumlahBuku; i++) {
-                    switch (kategori) {
 
-                        case 1:
-                            if (perpustakaan[i][6].equalsIgnoreCase("novel")) {
-                                System.out.println("_______________________________");
-                                System.out.println(" Judul          : " + perpustakaan[i][0]);
-                                System.out.println(" Penulis        : " + perpustakaan[i][1]);
-                                System.out.println(" Penerbit       : " + perpustakaan[i][2]);
-                                System.out.println(" Tahun Terbit   : " + perpustakaan[i][3]);
-                                System.out.println(" ISBN           : " + perpustakaan[i][4]);
-                                System.out.println(" Jumlah Halaman : " + perpustakaan[i][5]);
-                                System.out.println("_______________________________");
+                        for (int i = 0; i < jumlahBuku; i++) {
+                            switch (kategori) {
+                                case 1:
+                                if (perpustakaan[i][6].equalsIgnoreCase("novel")) {
+                                System.out.println("=========");
+                                System.out.println("  Novel ");
+                                System.out.println("=========");
+                                System.out.println(i+1 + ". " + perpustakaan[i][0]);
                                 input.nextLine();
-                            }
-                            break;
-
-                        case 2:
-                            if (perpustakaan[i][6].equalsIgnoreCase("komik")) {
-                                System.out.println("_______________________________");
-                                System.out.println(" Judul          : " + perpustakaan[i][0]);
-                                System.out.println(" Penulis        : " + perpustakaan[i][1]);
-                                System.out.println(" Penerbit       : " + perpustakaan[i][2]);
-                                System.out.println(" Tahun Terbit   : " + perpustakaan[i][3]);
-                                System.out.println(" ISBN           : " + perpustakaan[i][4]);
-                                System.out.println(" Jumlah Halaman : " + perpustakaan[i][5]);
-                                System.out.println("_______________________________");
+                                } 
+                                break;
+                                
+                            
+                                case 2:
+                                if (perpustakaan[i][6].equalsIgnoreCase("komik")) {
+                                System.out.println("=========");
+                                System.out.println("  Komik ");
+                                System.out.println("=========");
+                                System.out.println(i+1 + ". " + perpustakaan[i][0]);
                                 input.nextLine();
-                            }
+                                }
+                        
+                                break;
 
-                            break;
-
-                        case 3:
-                            if (perpustakaan[i][6].equalsIgnoreCase("cerpen")) {
-                                System.out.println("_______________________________");
-                                System.out.println(" Judul          : " + perpustakaan[i][0]);
-                                System.out.println(" Penulis        : " + perpustakaan[i][1]);
-                                System.out.println(" Penerbit       : " + perpustakaan[i][2]);
-                                System.out.println(" Tahun Terbit   : " + perpustakaan[i][3]);
-                                System.out.println(" ISBN           : " + perpustakaan[i][4]);
-                                System.out.println(" Jumlah Halaman : " + perpustakaan[i][5]);
-                                System.out.println("_______________________________");
+                                case 3:
+                                if (perpustakaan[i][6].equalsIgnoreCase("cerpen")) {
+                                System.out.println("==========");
+                                System.out.println("  Cerpen ");
+                                System.out.println("==========");
+                                System.out.println(i+1 + ". " + perpustakaan[i][0]);
                                 input.nextLine();
-                            }
-                            break;
+                                }
+                                break;
 
-                        case 4:
-                            if (perpustakaan[i][6].equalsIgnoreCase("sejarah")) {
-                                System.out.println("_______________________________");
-                                System.out.println(" Judul          : " + perpustakaan[i][0]);
-                                System.out.println(" Penulis        : " + perpustakaan[i][1]);
-                                System.out.println(" Penerbit       : " + perpustakaan[i][2]);
-                                System.out.println(" Tahun Terbit   : " + perpustakaan[i][3]);
-                                System.out.println(" ISBN           : " + perpustakaan[i][4]);
-                                System.out.println(" Jumlah Halaman : " + perpustakaan[i][5]);
-                                System.out.println("_______________________________");
+                                case 4:
+                                if (perpustakaan[i][6].equalsIgnoreCase("sejarah")) {
+                                System.out.println("===========");
+                                System.out.println("  Sejarah ");
+                                System.out.println("===========");
+                                System.out.println(i+1 + ". " + perpustakaan[i][0]);
                                 input.nextLine();
-                            }
-                            break;
+                                }
+                                break;
 
-                        case 5:
-                            if (perpustakaan[i][6].equalsIgnoreCase("ilmu pengetahuan")) {
-                                System.out.println("_______________________________");
-                                System.out.println(" Judul          : " + perpustakaan[i][0]);
-                                System.out.println(" Penulis        : " + perpustakaan[i][1]);
-                                System.out.println(" Penerbit       : " + perpustakaan[i][2]);
-                                System.out.println(" Tahun Terbit   : " + perpustakaan[i][3]);
-                                System.out.println(" ISBN           : " + perpustakaan[i][4]);
-                                System.out.println(" Jumlah Halaman : " + perpustakaan[i][5]);
-                                System.out.println("_______________________________");
+                                case 5:
+                                if (perpustakaan[i][6].equalsIgnoreCase("ilmu pengetahuan")) {
+                                System.out.println("====================");
+                                System.out.println("  Ilmu Pengetahuan ");
+                                System.out.println("====================");
+                                System.out.println(i+1 + ". " + perpustakaan[i][0]);
                                 input.nextLine();
+                                }
+                                break;
+
+                                
+                                default:
+                                System.out.println("Kategori Tidak Tersedia");
+                                break;
                             }
-                            break;
-
-                        default:
-                            System.out.println("Kategori Tidak Tersedia");
-                            break;
-
+                            
+                        }
+                    } else {
+                        System.out.println("\n=================================");
+                        System.out.println(" Belum ada buku yang ditambahkan ");
+                        System.out.println("=================================");
                     }
-                
                 }
-                
             }
-
-        } else {
-            System.out.println("\n=================================");
-            System.out.println(" Belum ada buku yang ditambahkan ");
-            System.out.println("=================================");
-        }
-}
-}
